5037844cbd98966171b911e412674577fdfd36ca
inkollusaipavani <42571330+inkollusaipavani@users.noreply.github.com>
2018-09-21 05:53:28 +0000

    Update 87.py

--- 87.py
+++ 87.py
@@ -1,8 +1,8 @@
 a,b=map(int,raw_input().split())
 def gcd(x,y):
-    z=abs(x-y)
+    k=abs(x-y)
     if (x-y)==0:
         return y
     else:
-        return gcd(z,min(x,y))
+        return gcd(k,min(x,y))
 print gcd(a,b)
