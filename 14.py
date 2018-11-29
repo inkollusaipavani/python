9d4f4526d44a4a9db7a1b323ff0f5ec63a439202
inkollusaipavani <42571330+inkollusaipavani@users.noreply.github.com>
2018-11-29 17:38:01 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,4 +1,4 @@
-g,h=map(int,raw_input().split())
-for z in range(g+1,h+1):
-  if(z%2!=0):
-    print(z),
+g1,h1=map(int,raw_input().split())
+for z in range(g1+1,h1+1):
+  if(a%2!=0):
+    print(a),
